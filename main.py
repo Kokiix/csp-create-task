d7938314eaeb4d257b2792b400e081ec04b00dd5
Kokiix <kokiidoki@gmail.com>
2022-03-02 06:25:26 -0500

    refactor tile setup to use tile class -on_tile_click still needs to be refactored -possible rename of prepare_default_tile

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import tkinter as tk
 import random as r
 import math
 
+
+
+
+class Tile(object):
+    def __init__(self, canvas, length, color, tile_type, row, col):
+        self.canvas = canvas
+        self.type = tile_type
+        self.row = row
+        self.col = col
+
+        x = self.col * length
+        y = self.row * length
+        self.tile_id = self.canvas.create_rectangle(
+            x, y,
+            x + length, y + length,
+            fill = color,
+            outline = "",
+            tags = ["tile", self.type])
+
+
+
+
 # inherit from frame; gui setup
 class Minesweeper(tk.Frame):
     def __init__(self, parent):
@@ -20,31 +42,8 @@ class Minesweeper(tk.Frame):
 
 
     def _clear_blank_tiles(self, tile_id, tile_column, tile_row):
-        self.canvas.delete(tile_id)
-        # for direction in range(0, 2):
-        #     column_or_row = tile_column if direction == 0 else tile_row
-        #     for i in range(-1 if column_or_row > 0 else 1, 2 if column_or_row < (len(self.minefield) - 1) else 0, 2):
-        #         tile_row = column_or_row + i if direction == 0 else tile_row
-        #         tile_column = column_or_row + i if direction == 1 else tile_column
-        #         scan_tile = self.minefield[tile_row][tile_column]
+        pass
 
-        #         tags = self.canvas.gettags(scan_tile)
-        #         if len(tags) != 0 and "bomb" not in tags:
-        #             self._clear_blank_tiles(scan_tile, tile_column + i, tile_row)
-
-
-        for i in range(-1 if tile_column > 0 else 1, 2 if tile_column < (len(self.minefield) - 1) else 0, 2):
-            scan_tile = self.minefield[tile_row][tile_column + i]
-            tags = self.canvas.gettags(scan_tile)
-            if len(tags) != 0 and "bomb" not in tags:
-                print(str(tile_row) + " " + str(tile_column + i))
-                self._clear_blank_tiles(scan_tile, tile_column + i, tile_row)
-                
-        for i in range(-1 if tile_row > 0 else 1, 2 if tile_row < (len(self.minefield) - 1) else 0, 2):
-            scan_tile = self.minefield[tile_row + i][tile_column]
-            tags = self.canvas.gettags(scan_tile)
-            if len(tags) != 0 and "bomb" not in tags:
-                self._clear_blank_tiles(scan_tile, tile_column, tile_row + i)
 
 
     def _on_tile_click(self, event):
@@ -54,6 +53,7 @@ class Minesweeper(tk.Frame):
 
         if "blank" in self.canvas.gettags(tile_id):
             self._clear_blank_tiles(tile_id, tile_column, tile_row)
+
 
 
     def _prepare_default_tile(self, row, col):
@@ -67,16 +67,18 @@ class Minesweeper(tk.Frame):
 
         is_bomb = True if r.random() > 0.10 else False # temporary bomb assignment for debug
         
-        tile_id = self.canvas.create_rectangle(
-            x, y, x + self.tile_length, y + self.tile_length, 
-            fill = fill_color if is_bomb else "#53131E", outline = "", tags = "tile") # bombs temporarily highlighted for debug
-        self.canvas.addtag_withtag("bomb" if not is_bomb else "blank", tile_id)
+        return Tile(
+            self.canvas, 
+            self.tile_length, 
+            fill_color if is_bomb else "#53131E", # debug outline
+            "bomb" if not is_bomb else "blank",
+            row, col)
 
-        return tile_id
 
 
     def _create_minefield(self):
         self.minefield = [[self._prepare_default_tile(row, col) for col in range(10)] for row in range(10)]
+
 
 
 
